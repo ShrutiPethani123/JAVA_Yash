cd80fee16cbfeae4fe4108e42180790349099390
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2023-01-13 12:53:24 +0530

    data type

---
+++ DataType.java
@@ -0,0 +1,61 @@
+// single line comment
+
+/*
+Multi line comment
+
+*/
+
+/*
+Data types:
+
+boolean - true , false
+byte
+short
+int
+long
+float
+double
+char
+
+String - class
+
+
+*/
+
+
+class DataType{
+
+	public static void main(String args[])
+	{
+		int a= 45;
+		int b=60;
+		System.out.println(a + b);
+		System.out.println("Addition is: " + a + b);
+		System.out.println(a+b +  " Addition is: " + (a + b));
+		
+		boolean c = true;
+		System.out.println(c);
+
+		float f = 45.60F;// f or F
+		System.out.println(f);
+
+		double d = 5474.7347;
+		System.out.println(d);
+			
+		long s = 865828657568l; // L or l
+		System.out.println(s);
+		
+
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ Test.java
@@ -0,0 +1,27 @@
+class A{
+
+	public static void main(String [] yash)
+	{
+		System.out.println("Class A");
+	}
+
+}
+
+class B{
+
+	public static void main(String [] yash)
+	{
+		System.out.println("Class B");
+	}
+
+}
+
+
+class C{
+
+	public static void main(String [] yash)
+	{
+		System.out.println("Class C");
+	}
+
+}
